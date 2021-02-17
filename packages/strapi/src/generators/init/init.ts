a7a0eec9860e71a8510764c76bdf3fce17809744
Vimal Patel <vimal.patel.nvs@gmail.com>
2021-02-17 11:23:05 +0530

    Add required dependency for Strapi

--- packages/strapi/src/generators/init/init.ts
+++ packages/strapi/src/generators/init/init.ts
@@ -1,4 +1,4 @@
-import { chain, noop, Rule } from '@angular-devkit/schematics';
+import { chain, noop, Rule,Tree } from '@angular-devkit/schematics';
 import { Schema } from './schema';
 import { addDepsToPackageJson, addPackageWithInit } from '@nrwl/workspace';
 
@@ -8,14 +8,19 @@ export default function (schema: Schema): Rule {
       ? addPackageWithInit('@nrwl/jest')
       : noop(),
     addDepsToPackageJson(
-      {},
       {
-        'svelte-jester': '^1.3.0',
-        'svelte': '^3.31.0',
-        'svelte-check': '^1.1.20',
-        'svelte-preprocess': '^4.6.1',
-        '@tsconfig/svelte': '^1.0.0'
-      }
+        "strapi": "3.4.6",
+        "strapi-admin": "3.4.6",
+        "strapi-utils": "3.4.6",
+        "strapi-plugin-content-type-builder": "3.4.6",
+        "strapi-plugin-content-manager": "3.4.6",
+        "strapi-plugin-users-permissions": "3.4.6",
+        "strapi-plugin-email": "3.4.6",
+        "strapi-plugin-upload": "3.4.6",
+        "strapi-connector-bookshelf": "3.4.6",
+        "knex": "<0.20.0",
+        "sqlite3": "5.0.0"
+      },{}
     ),
   ]);
 }
